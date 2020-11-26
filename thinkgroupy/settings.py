5a298ba32503b0d05bc00452434b4b5941f06196
saurabh <singhjdsaurabh@gmail.com>
2020-11-26 07:37:57 +0000

    update from server

--- thinkgroupy/settings.py
+++ thinkgroupy/settings.py
@@ -8,8 +8,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 SECRET_KEY = config("SECRET_KEY")
 
-#DEBUG = config('DEBUG', default=True, cast=bool)
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
+
 ALLOWED_HOSTS = ['*']
 
 APPEND_SLASH = False
