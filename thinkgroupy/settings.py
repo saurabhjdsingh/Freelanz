13b61dd20c470094a59860335f716cdde16bc57b
Saurabh singh <saurabhjdsingh@users.noreply.github.com>
2021-01-09 01:47:30 +0530

    s3

--- thinkgroupy/settings.py
+++ thinkgroupy/settings.py
@@ -127,9 +127,20 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 ]
 
-MEDIA_URL = '/media/'
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+PUBLIC_MEDIA_LOCATION = 'media'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+DEFAULT_FILE_STORAGE = 'thinkgroupy.storage_backends.PublicMediaStorage'
+
+
+#MEDIA_URL = '/media/'
+
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 SOCIALACCOUNT_PROVIDERS = {
