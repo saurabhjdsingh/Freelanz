796754e8a506087353f3f762fe621387ac452a57
Saurabh singh <66870178+saurabhjdsingh@users.noreply.github.com>
2021-04-19 22:32:53 +0530

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -9,9 +9,11 @@ urlpatterns = [
     path("my", views.Myprojects, name="my_project"),
     path("new/", views.CreateProject, name="create"),
     path("category", views.category, name="category"),
+    path("buy_emails", views.buy_emails, name="buy_emails"),
     path("<str:category>", views.Category_Project, name="category_project"),
     path("project/<str:slug>", views.Slug_Project, name="slug_project"),
     path("apply/<slug:project>", views.BidProject, name="bid_project"),
     path("bids/<slug:project>", views.Applicants, name="applicants"),
     path("orders/", views.orders, name="orders")
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
