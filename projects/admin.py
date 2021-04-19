7bfde554b8ed4b784289fb2754319698e0cf8737
Saurabh singh <66870178+saurabhjdsingh@users.noreply.github.com>
2021-04-19 22:31:48 +0530

    Update admin.py

--- projects/admin.py
+++ projects/admin.py
@@ -1,40 +1,26 @@
 from django.contrib import admin
 from django.contrib.admin.options import ModelAdmin
-from .models import Project, Bid, categories, Order, CompletedOrder
+from .models import Project, Bid, categories, Order, CompletedOrder, EmailSubscription, EmailRequest
 
 class OrderAdmin(ModelAdmin):
     list_display = ["creator", "worker", "bid"]
     search_fields = ["creator", "worker", "bid"]
     list_filter = ["creator", "worker", "bid"]
 
-
-admin.site.register(Order, OrderAdmin)
-
 class CompletedOrderAdmin(ModelAdmin):
     list_display = ["user", "order", "completed_on"]
     search_fields = ["user", "order", "completed_on"]
     list_filter = ["user", "order", "completed_on"]
-
-
-admin.site.register(CompletedOrder, CompletedOrderAdmin)
 
 class ProjectAdmin(ModelAdmin):
     list_display = ["name", "category", "created_by"]
     search_fields = ["name", "category", "created_by", "members"]
     list_filter = ["name", "category", "created_by"]
 
-
-admin.site.register(Project, ProjectAdmin)
-
-
 class BidAdmin(ModelAdmin):
     list_display = ["budget", "description", "created_by"]
     search_fields = ["budget", "description", "created_by"]
     list_filter = ["budget", "description", "created_by"]
-
-
-admin.site.register(Bid, BidAdmin)
-
 
 class categoriesAdmin(ModelAdmin):
     list_display = ["name", "image"]
@@ -42,4 +28,23 @@ class categoriesAdmin(ModelAdmin):
     list_filter = ["name", "image"]
 
 
+class EmailSubscriptionAdmin(ModelAdmin):
+    list_display = ["profile", "email_type", "number_of_emails", "no_of_emails_send"]
+    search_fields = ["profile__user__username", "email_type"]
+    list_filter = ["email_type"]
+    
+class EmailRequestAdmin(ModelAdmin):
+    list_display = ["profile","expiry_date", "completed", "number_of_emails"]
+    search_fields = ["profile__user__username"]
+    list_filter = ["completed", "expiry_date"]
+
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(CompletedOrder, CompletedOrderAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Bid, BidAdmin)
 admin.site.register(categories, categoriesAdmin)
+admin.site.register(EmailSubscription, EmailSubscriptionAdmin)
+admin.site.register(EmailRequest, EmailRequestAdmin)
+
