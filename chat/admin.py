b38f09a59c8acc3b2c4552226cdd7b5eded8294d
Saurabh singh <66870178+saurabhjdsingh@users.noreply.github.com>
2021-05-08 17:04:33 +0530

    Update admin.py

--- chat/admin.py
+++ chat/admin.py
@@ -5,7 +5,7 @@ from .models import RoomId, Message, ChatFile, DeliverFiles, Notify
 
 class RoomIdAdmin(ModelAdmin):
     list_display = ["user1", "user2", "room_name"]
-    search_fields = ["user1", "user2", "room_name"]
+    search_fields = ["user1__username", "user2__username", "room_name"]
     list_filter = ["user1", "user2", "room_name"]
 
 
@@ -13,8 +13,8 @@ admin.site.register(RoomId, RoomIdAdmin)
 
 
 class MessageAdmin(ModelAdmin):
-    list_display = ["key", "created_on"]
-    search_fields = ["key", "created_on"]
+    list_display = ["author", "key", "created_on"]
+    search_fields = ["key__room_name", "author__username"]
     list_filter = ["key", "created_on"]
 
 
@@ -22,7 +22,7 @@ admin.site.register(Message, MessageAdmin)
 
 class NotifyAdmin(ModelAdmin):
     list_display = ["read", "user"]
-    search_fields = ["read", "user"]
+    search_fields = ["read", "user__username"]
     list_filter = ["read", "user"]
 
 
@@ -30,7 +30,7 @@ admin.site.register(Notify, NotifyAdmin)
 
 class ChatFileAdmin(ModelAdmin):
     list_display = ["FileFrom", "FileTo"]
-    search_fields = ["FileFrom", "FileTo"]
+    search_fields = ["FileFrom__username", "FileTo__username"]
     list_filter = ["FileFrom", "FileTo"]
 
 
@@ -38,7 +38,7 @@ admin.site.register(ChatFile, ChatFileAdmin)
 
 class DeliverFilesAdmin(ModelAdmin):
     list_display = ["FileFrom", "FileTo"]
-    search_fields = ["FileFrom", "FileTo"]
+    search_fields = ["FileFrom__username", "FileTo__username"]
     list_filter = ["FileFrom", "FileTo"]
 
 
