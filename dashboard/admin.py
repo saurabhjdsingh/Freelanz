26cb9f1e474365337c0c84c1c6b9d608ea351600
Saurabh singh <66870178+saurabhjdsingh@users.noreply.github.com>
2021-05-08 17:05:49 +0530

    Update admin.py

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -5,7 +5,7 @@ from dashboard.models import Profile, FollowUser, PrimaryEmail, UserPhone
 
 class ProfileAdmin(ModelAdmin):
     list_display = ["user", "gender", "location", "birth_day"]
-    search_fields = ["user", "gender", "location", "phone"]
+    search_fields = ["user__username", "gender", "location"]
     list_filter = ["gender", "location"]
 
 
@@ -13,7 +13,7 @@ admin.site.register(Profile, ProfileAdmin)
 
 class UserPhoneAdmin(ModelAdmin):
     list_display = ["verified","user", "phone"]
-    search_fields = ["user", "phone"]
+    search_fields = ["user__username", "phone"]
     list_filter = ["verified","user", "phone"]
 
 
@@ -22,7 +22,7 @@ admin.site.register(UserPhone, UserPhoneAdmin)
 
 class PrimaryEmailAdmin(ModelAdmin):
     list_display = ["user", "email"]
-    search_fields = ["user", "email"]
+    search_fields = ["user__username", "email"]
     list_filter = ["user", "email"]
 
 
@@ -31,7 +31,7 @@ admin.site.register(PrimaryEmail, PrimaryEmailAdmin)
 
 class FollowUserAdmin(ModelAdmin):
     list_display = ["profile", "Followed_by"]
-    search_fields = ["profile", "Followed_by"]
+    search_fields = ["profile__user__username", "Followed_by__username"]
     list_filter = ["profile", "Followed_by"]
 
 
