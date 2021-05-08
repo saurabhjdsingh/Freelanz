8616022f23257aa331dd47a09101879851f14488
Saurabh singh <66870178+saurabhjdsingh@users.noreply.github.com>
2021-05-08 17:07:42 +0530

    Update admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -5,7 +5,7 @@ from posts.models import MyPost, PostComment, PostLike
 
 class MyPostAdmin(ModelAdmin):
     list_display = ["subject", "created_at", "uploaded_by"]
-    search_fields = ["subject", "message", "uploaded_by"]
+    search_fields = ["subject", "message", "uploaded_by__user__username"]
     list_filter = ["created_at", "uploaded_by"]
 
 
@@ -14,7 +14,7 @@ admin.site.register(MyPost, MyPostAdmin)
 
 class PostCommentAdmin(ModelAdmin):
     list_display = ["post", "message"]
-    search_fields = ["post", "message", "comented_by"]
+    search_fields = ["post__id", "message", "comented_by__username"]
     list_filter = ["created_at", "flag"]
 
 
@@ -23,11 +23,8 @@ admin.site.register(PostComment, PostCommentAdmin)
 
 class PostLikeAdmin(ModelAdmin):
     list_display = ["post", "Liked_by"]
-    search_fields = ["post", "Liked_by"]
+    search_fields = ["post__id", "Liked_by__username"]
     list_filter = ["created_at"]
 
 
 admin.site.register(PostLike, PostLikeAdmin)
-
-
-
