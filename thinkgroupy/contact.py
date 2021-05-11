4866bac1acdf1afd2c649518b2b15f2fd38f6c84
Saurabh singh <66870178+saurabhjdsingh@users.noreply.github.com>
2021-05-11 22:09:14 +0530

    Update contact.py

--- thinkgroupy/contact.py
+++ thinkgroupy/contact.py
@@ -5,7 +5,8 @@ from django.template.loader import render_to_string
 from django.core.mail import EmailMultiAlternatives
 from django.utils.html import strip_tags
 from thinkgroupy import settings
-
+from .settings import RECAPTCHA_SITE_KEY, RECAPTCHA_SECRET_KEY
+import requests, json
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control' }))
@@ -17,29 +18,38 @@ class ContactForm(forms.Form):
 def contact(request):
     submitted = False
     if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            html_content = render_to_string('contact_email.html', {
-                'name': cd['name'],
-                'subject': cd['subject'],
-                'message': cd['message'],
-                'from_email': cd['email']
-            })
-            text_content = strip_tags(html_content)
-            email = EmailMultiAlternatives(
-                'Message From Thinkgroupy',
-                text_content,
-                settings.EMAIL_HOST_USER,
-                ['singhjdsaurabh@gmail.com'])
-            email.attach_alternative(html_content, "text/html")
-            email.send()
-            return HttpResponseRedirect('/contact?submitted=True')
+        clientkey=request.POST['g-recaptcha-response']
+        capchtaData = {
+            'secret':RECAPTCHA_SECRET_KEY,
+            'response':clientkey
+        }
+        r = requests.post('https://www.google.com/recaptcha/api/siteverify',data=capchtaData)
+        response = json.loads(r.text)
+        verify = response['success']
+        if verify:
+            form = ContactForm(request.POST)
+            if form.is_valid():
+                cd = form.cleaned_data
+                html_content = render_to_string('contact_email.html', {
+                    'name': cd['name'],
+                    'subject': cd['subject'],
+                    'message': cd['message'],
+                    'from_email': cd['email']
+                })
+                text_content = strip_tags(html_content)
+                email = EmailMultiAlternatives(
+                    'Message From Thinkgroupy',
+                    text_content,
+                    settings.EMAIL_HOST_USER,
+                    ['singhjdsaurabh@gmail.com'])
+                email.attach_alternative(html_content, "text/html")
+                email.send()
+                return HttpResponseRedirect('/contact?submitted=True')
+        else:
+            return render(request, 'index.html')
     else:
         form = ContactForm()
         if 'submitted' in request.GET:
             submitted = True
-
     return render(request, 'contact.html',
-                  {'form': form, 'submitted': submitted})
-
+                  {'form': form, 'submitted': submitted, 'RECAPTCHA_SITE_KEY': RECAPTCHA_SITE_KEY})
